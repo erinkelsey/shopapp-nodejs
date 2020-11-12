1ce602625bbafc7ff0d366489b50a9a43e625612
erinkelsey <erinszilagyi@gmail.com>
2020-11-11 18:40:06 -0600

    Add main, add product, and 404 pages with styling

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const express = require("express");
+const path = require('path')
 
-const app = express();
+const express = require("express")
+const bodyParser = require('body-parser') 
 
-app.use((req, res, next) => {});
+const app = express()
 
-app.listen(3000);
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
+
+app.listen(3000)
---
+++ routes/admin.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+const express = require('express')
+
+const rootDir = require('../util/path')
+
+const router = express.Router()
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, '..', 'views', 'add-product.html'))
+})
+
+// /admin/product => POST
+router.post('/product', (req, res, next) => {
+  console.log(req.body)
+  res.redirect('/')
+})
+
+module.exports = router
