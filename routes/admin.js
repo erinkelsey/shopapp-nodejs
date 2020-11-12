c2a0e1b3ab8b2d023d7b99c305ef784dc07731a2
erinkelsey <erinszilagyi@gmail.com>
2020-11-12 12:10:32 -0600

    Add EJS templates

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,19 @@
-const path = require('path')
-
 const express = require('express')
-
-const rootDir = require('../util/path')
 
 const router = express.Router()
 
+const products = []
+
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, '..', 'views', 'add-product.html'))
+  res.render('add-product', { pageTitle: 'Add Product' })
 })
 
 // /admin/product => POST
-router.post('/product', (req, res, next) => {
-  console.log(req.body)
+router.post('/add-product', (req, res, next) => {
+  products.push({'title': req.body.title})
   res.redirect('/')
 })
 
-module.exports = router
+exports.routes = router
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -2,13 +2,15 @@ const path = require('path')
 
 const express = require('express')
 
-const rootDir = require('../util/path')
+const adminData = require('./admin')
 
 const router = express.Router()
 
-
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, '..', 'views', 'shop.html'))
+  res.render('shop', {
+    prods: adminData.products,
+    pageTitle: 'Shop',
+  })
 })
 
 module.exports = router
