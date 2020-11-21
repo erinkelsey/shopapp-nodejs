a4a4f4a3ea1288a3e278910dc8c7320a92f3735d
erinkelsey <erinszilagyi@gmail.com>
2020-11-21 03:57:50 -0600

    Add pagination to shop product pages

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,40 +6,77 @@ const PDFDocument = require('pdfkit')
 const Product = require('../models/product')
 const Order = require('../models/order')
 
+const ITEMS_PER_PAGE = 2
+
 /**
  * Controller for rendering the shop view. 
  * 
  * The main index view for the app.
+ * 
+ * Pagination for viewing only a chunk of products. 
  */
 exports.getIndex = (req, res, next) => {
-  Product
-    .find()
-    .then(products => {
-      res.render('shop/index', {
-        products: products,
-        pageTitle: 'Shop',
-        path: '/'
-      })
+  const page = +req.query.page || 1
+  let totalItems = 0
+
+  Product.find().countDocuments().then(numProducts => {
+    totalItems = numProducts
+    return Product
+            .find()
+            .skip((page - 1) * ITEMS_PER_PAGE)
+            .limit(ITEMS_PER_PAGE)
+  })
+  .then(products => {
+    res.render('shop/index', {
+      products: products,
+      pageTitle: 'Shop',
+      path: '/',
+      currentPage: page,
+      hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+      hasPreviousPage: page > 1,
+      nextPage: page + 1,
+      previousPage: page - 1,
+      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
     })
-    .catch(err => {
-      const error = new Error(err)
-      error.httpStatusCode = 500
-      return next(error)
-    })
+  })
+  .catch(err => {
+    console.log(err);
+    const error = new Error(err)
+    error.httpStatusCode = 500
+    return next(error)
+  })
 }
 
 /**
  * Controller for rendering the view containing all of the
  * products of the shop. 
+ * 
+ * Pagination for viewing only a chunk of products. 
  */
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1
+  let totalItems = 0
   Product
     .find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts
+      return Product
+            .find()
+            .skip((page - 1) * ITEMS_PER_PAGE)
+            .limit(ITEMS_PER_PAGE)
+    })
     .then(products => {
       res.render('shop/product-list', {
         products: products,
         pageTitle: 'All Products',
-        path: '/products'
+        path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       })
     })
     .catch(err => {
